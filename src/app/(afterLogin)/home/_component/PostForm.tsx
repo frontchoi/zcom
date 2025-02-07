dbcc5a31df8bb76bb2f1f77bf3ea4ae44d442e8d
Soon-cheol <choisc@penta.co.kr>
2025-02-07 14:48:26 +0900

    test

--- src/app/(afterLogin)/home/_component/PostForm.tsx
+++
@@ -1,3 +0,0 @@
-export default function PostForm() {
-  return <div>PostForm</div>;
-}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,14 +1,25 @@
+import Post from '../_component/Post';
+import PostForm from '../_component/PostForm';
 import Tab from './_component/Tab';
 import TabProvider from './_component/TabProvider';
 import style from './home.module.css';
 
 export default function Home() {
-  return (
-    <div className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-      </TabProvider>
-    </div>
-  );
+    return (
+        <div className={style.main}>
+            <TabProvider>
+                <Tab />
+                <PostForm />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </TabProvider>
+        </div>
+    );
 }
