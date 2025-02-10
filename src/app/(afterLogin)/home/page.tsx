5ff6e38e75e2edbaacda74f9fc1bd3964b127e14
DG-CHOISC\DG-Choi SL <choisc@penta.co.kr>
2025-02-10 10:21:23 +0900

    test

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,25 +1,25 @@
 import Post from '../_component/Post';
-import PostForm from '../_component/PostForm';
+import PostForm from './_component/PostForm';
 import Tab from './_component/Tab';
 import TabProvider from './_component/TabProvider';
 import style from './home.module.css';
 
 export default function Home() {
-    return (
-        <div className={style.main}>
-            <TabProvider>
-                <Tab />
-                <PostForm />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-            </TabProvider>
-        </div>
-    );
+  return (
+    <div className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </div>
+  );
 }
